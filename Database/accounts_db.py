ffc2d9634332bbb3a64a9a93ba73c172a7038f82
Fink József <erronblack0451@gmail.com>
2024-11-02 11:55:52 +0100

    Felhasználói tábla létrehozása

--- Database/accounts_db.py
+++ Database/accounts_db.py
@@ -3,3 +3,13 @@ import sqlite3
 # csatlakozás az adatbázishoz vagy létrehozás ha nem létezik
 conn = sqlite3.connect('user_data.db')
 cursor = conn.cursor()
+
+# Felhasználói tábla létrehozása
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    credit REAL DEFAULT 0
+)
+''')
