feac54d0f115407cee207febe58052a1af0c78c1
Fink József <erronblack0451@gmail.com>
2024-11-02 12:11:15 +0100

    adatbázishoz kapcsolódódáshoz segítő funkció

--- Back_end/app.py
+++ Back_end/app.py
@@ -1,2 +1,11 @@
 from flask import Flask, request, redirect, render_template, url_for, flash
 import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # ide kell sajat tikos kulcs
+
+# adatbázishoz kapcsolódódáshoz segítő funkció
+def get_db_connection():
+    conn = sqlite3.connect('user_data.db')
+    conn.row_factory = sqlite3.Row
+    return conn
