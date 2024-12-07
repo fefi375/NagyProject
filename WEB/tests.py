1576c9c899625e250aa2e8aea195aaa53f932c66
Szűcs Ferenc <fefi375@gmail.com>
2024-12-07 14:32:30 +0100

    login, account_create endpoint ok response tests

--- WEB/tests.py
+++ WEB/tests.py
@@ -6,13 +6,32 @@ import app
 
 class TestApi(unittest.TestCase):
     
-    def test_okResponse(self):
-      testApp=app.app
-      tester = testApp.test_client(self)
-      response=tester.get('/')
+    def setUp(self):
+        self.testapp=app.app
+        self.tester = self.testapp.test_client(self)
+        
+        
+        return self
+    
+    def tearDown(self):
+        return super().tearDown()
+   
+   
+    def test_okresponse_on_homepage(self):
+      response=self.tester.get('/')
       statuscode =response.status_code
       self.assertEqual(statuscode, 200)
       
+    def test_okresponse_on_login(self):
+        response=self.tester.get('/login') 
+        statuscode =response.status_code
+        self.assertEqual(statuscode, 200)
+        
+    def test_account_create(self):
+        response=self.tester.get('/account_create')
+        statuscode =response.status_code
+        self.assertEqual(statuscode, 200)
+    
       
 if __name__=="__main__":
     unittest.main()
